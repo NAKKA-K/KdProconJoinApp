b6b3138d29d476f7de4e857f02956f5ad4d30463
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-12-12 17:27:41 +0900

    Add DBへのデータ追加、データのJSON取得を実装

--- app/src/main/java/com/example/snakka/kdproconjoinapp/SharedPreferenceAccessor.java
+++ app/src/main/java/com/example/snakka/kdproconjoinapp/SharedPreferenceAccessor.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import java.util.HashMap;
+
 public class SharedPreferenceAccessor {
-    private static final String DEPARTMENT_KEY = "department_key";
-    private static final String SCHOOL_YEAR_KEY = "school_year_key";
-    private static final String CLASS_NO_KEY = "class_no_key";
-    private static final String FIRST_NAME_KEY = "first_name_key";
-    private static final String FAMILY_NAME_KEY = "family_name_key";
+    public static final String DEPARTMENT_KEY = "department_key";
+    public static final String SCHOOL_YEAR_KEY = "school_year_key";
+    public static final String CLASS_NO_KEY = "class_no_key";
+    public static final String FIRST_NAME_KEY = "first_name_key";
+    public static final String FAMILY_NAME_KEY = "family_name_key";
 
 
     public static Boolean isRegisteredUser(){
@@ -25,11 +27,31 @@ public class SharedPreferenceAccessor {
         return true;
     }
 
-    public static void commitUserStatusStr(String department, String schoolYear, String classNo, String firstName, String familyName){
+    public static Boolean commitUserStatusStr(HashMap<String, String> userStatus){
+        if(userStatus.isEmpty())
+            return false;
 
-        if(department != null){
+        SharedPreferences.Editor editor =
+                PreferenceManager.getDefaultSharedPreferences(AppContextMgr.getContext()).edit();
 
+        String[] REFERENCE_KEYS = {DEPARTMENT_KEY, SCHOOL_YEAR_KEY, CLASS_NO_KEY, FIRST_NAME_KEY, FAMILY_NAME_KEY};
+        for(String key:REFERENCE_KEYS){
+            if(userStatus.containsKey(key)){
+                editor.putString(key, userStatus.get(key));
+            }
         }
+        return true;
+    }
 
+    public static String getUserOfJson(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(AppContextMgr.getContext());
+
+        return "{" +
+                "\"department\":\"" + preferences.getString(DEPARTMENT_KEY, null) + "\"," +
+                "\"schoolYear\":" + Integer.parseInt(preferences.getString(SCHOOL_YEAR_KEY, "0")) + "," +
+                "\"classno\":" + Integer.parseInt(preferences.getString(CLASS_NO_KEY, "0")) + "," +
+                "\"familyName\":\"" + preferences.getString(FAMILY_NAME_KEY, null) + "\"," +
+                "\"firstName\":\"" + preferences.getString(FIRST_NAME_KEY, null) + "\"" +
+                "}";
     }
 }
