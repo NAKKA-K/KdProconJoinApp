ea449889a271a727a9c2fddb5f2a58d4d9c98071
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2017-12-12 18:46:48 +0900

    Add メイン画面で使用するデータをmap形式にして返すメソッドを追加

--- app/src/main/java/com/example/snakka/kdproconjoinapp/SharedPreferenceAccessor.java
+++ app/src/main/java/com/example/snakka/kdproconjoinapp/SharedPreferenceAccessor.java
@@ -12,6 +12,7 @@ public class SharedPreferenceAccessor {
     public static final String CLASS_NO_KEY = "class_no_key";
     public static final String FIRST_NAME_KEY = "first_name_key";
     public static final String FAMILY_NAME_KEY = "family_name_key";
+    static final String[] REFERENCE_KEYS = {DEPARTMENT_KEY, SCHOOL_YEAR_KEY, CLASS_NO_KEY, FIRST_NAME_KEY, FAMILY_NAME_KEY};
 
 
     public static Boolean isRegisteredUser(){
@@ -34,7 +35,6 @@ public class SharedPreferenceAccessor {
         SharedPreferences.Editor editor =
                 PreferenceManager.getDefaultSharedPreferences(AppContextMgr.getContext()).edit();
 
-        String[] REFERENCE_KEYS = {DEPARTMENT_KEY, SCHOOL_YEAR_KEY, CLASS_NO_KEY, FIRST_NAME_KEY, FAMILY_NAME_KEY};
         for(String key:REFERENCE_KEYS){
             if(userStatus.containsKey(key)){
                 editor.putString(key, userStatus.get(key));
@@ -54,4 +54,17 @@ public class SharedPreferenceAccessor {
                 "\"firstName\":\"" + preferences.getString(FIRST_NAME_KEY, null) + "\"" +
                 "}";
     }
+
+    public static HashMap<String, String> getUserStatus(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(AppContextMgr.getContext());
+
+        HashMap<String, String> userStatus = new HashMap<>();
+        for(String key:REFERENCE_KEYS){
+            if(preferences.contains(key))
+                userStatus.put(key, preferences.getString(key, null));
+        }
+
+        return userStatus;
+    }
+
 }
